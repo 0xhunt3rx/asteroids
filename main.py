e24c275564822558586ce6535150d5c943697731
0xhunt3rx <0xhunt3rx@gmail.com>
2025-05-28 13:52:52 +0100

    Add delta time feature

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock  = pygame.time.Clock()
+    dt     = 0
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +15,8 @@ def main():
 
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick() / 1000
 
 if __name__ == "__main__":
     main()
